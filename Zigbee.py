c132a5791ed99a2aa752e3ebf5233b7f13c51a49
Michal <michallingas@gmail.com>
2021-08-04 18:49:19 +0200

    v 0.2.10

--- Zigbee.py
+++ Zigbee.py
@@ -94,3 +94,57 @@ def get_zigbee_command(gui_values: tuple, text_output: PySimpleGUI.Input):
         command = command + addrAmodule + " "     # there is no need to add space sign because it was used to differ
         # commands
         text_output.update(command)
+
+
+def Calculate_CRC8_lookUpTable(lookUpTable: list, polynomial: int, data_format: str):
+
+    """
+    :param lookUpTable: destination list object where generated look up table will be stored
+    :param polynomial: CRC polynomial
+    :param data_format: defines data format
+        - bin - binary data presentation
+        - hex - hexadecimal data format
+        - decimal - by default, decimal format
+
+    """
+    for div in range(0, 256, 1):
+        byte = div
+
+        for bit in range(0, 8, 1):
+
+            if byte & 0x80 != 0:
+
+                byte = (byte << 1) ^ polynomial
+                if byte > 255:
+                    byte = byte & 0x00FF
+
+            else:
+
+                byte = byte << 1
+                if byte > 255:
+                    byte = byte & 0x00FF
+
+        if data_format == 'bin':
+            lookUpTable.append(bin(byte))
+
+        elif data_format == 'hex':
+            lookUpTable.append(hex(byte))
+
+        else:
+            lookUpTable.append(byte)
+
+
+def Compute_CRC8(data: str, lookUpTable: list):
+
+    """
+    :param data: input data
+    :param lookUpTable: look up table which stores computed CRC 8 values for given polynomial
+    :return: computed CRC-8 for input data
+    """
+    crc = 0
+
+    for i in data:
+        xor_in = i ^ crc
+        crc = lookUpTable[xor_in]
+
+    return crc
